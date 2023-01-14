b328014816640aa68330bfa68b20c35506f9ab29
Martin <mdsugarev@gmail.com>
2023-01-15 01:20:34 +0200

    added card component

--- brunchy/src/App.js
+++ brunchy/src/App.js
@@ -3,12 +3,13 @@ import HeaderTitle from './components/header/HeaderTitle';
 import Button from './components/button/Button';
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
+import Card from './components/card/Card';
 
 
 function App() {
   return (
     <div className={style['app']}>
-      <Hero text1={'The fastest brunch delivery in'} text2={'Your city'} />
+      <Card title={'Butter Pancakes'} subTitle={'With honey'} price={'14.30'} />
     </div>
   );
 }
--- brunchy/src/components/card/Card.jsx
+++ brunchy/src/components/card/Card.jsx
@@ -2,10 +2,16 @@ import style from './Card.module.scss';
 import React from 'react'
 import { memo } from 'react';
 
-const Card = () => {
+const Card = ({image = '/Asset 5.png', name, caption, price}) => {
     return (
         <div className={style['card']}>
-            
+            <img className={style['card-image']} src={image} alt="card-image" />
+            <div className={style['card-info']}>
+              <h6>{name}</h6>
+              <p className={style['card-subtitle']}>{caption}</p>
+               <p className={style['price']}>{price}</p>
+            </div>
+            <img className={style['card-basket']} src="/shopping-basket.png" alt="shopping-basket" />
         </div>
     )
 }
